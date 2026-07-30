6608997810bbae06972513373ce9cf823c7974d2
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-07-30 11:52:29 +0300

    Time: 733 ms (5.04%), Space: 19.5 MB (83.01%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,10 +5,14 @@ class Solution:
         pair={names[i]:heights[i] for i in range(m)}
         
         for i in range(n):
+           
             for j in range(n-1-i):
                 if heights[j]<heights[j+1]:
+              
                     heights[j],heights[j+1]=heights[j+1],heights[j]
                     names[j], names[j + 1] = names[j + 1], names[j]
+         
+             
         return names
 
         
